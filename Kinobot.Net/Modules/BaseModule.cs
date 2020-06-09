12850a919ac521bcf51805d0b1b55367245e4fb6
Oronar <kirby91@gmail.com>
2020-06-09 18:55:49 -0400

    fix: #3 - Add BaseModule with generic exception handling

---
+++ Kinobot.Net/Modules/BaseModule.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using Kinobot.Net.Services.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace Kinobot.Net.Modules
+{
+	public class BaseModule : ModuleBase<SocketCommandContext>
+	{
+		protected readonly ILoggingService loggingService;
+
+		public BaseModule(ILoggingService loggingService)
+		{
+			this.loggingService = loggingService;
+		}
+
+		public async Task ExecuteAsync(Func<Task> command)
+		{
+			if (command == null)
+			{
+				throw new ArgumentNullException($"{nameof(command)} cannot be null.");
+			}
+
+			try
+			{
+				await command.Invoke();
+			}
+			catch (Exception e)
+			{
+				await loggingService.LogAsync(e.ToString());
+				await ReplyAsync($"Error encountered. Please report this issue.");
+
+				throw;
+			}
+		}
+	}
+}
