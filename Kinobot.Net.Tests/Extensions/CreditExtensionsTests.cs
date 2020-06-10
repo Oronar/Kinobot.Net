4441549fbe45f185d16f94bfc3dc1081bb0bc8bf
Oronar <kirby91@gmail.com>
2020-06-10 17:33:57 -0400

    test: #12 - Add CreditExtensionsTests

---
+++ Kinobot.Net.Tests/Extensions/CreditExtensionsTests.cs
@@ -0,0 +1,80 @@
+﻿using Kinobot.Net.Extensions;
+using Kinobot.Net.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Kinobot.Net.Tests.Extensions
+{
+	public class CreditExtensionsTests
+	{
+		[Fact]
+		public void GetRoles_WithMatchingRoles_ReturnsRoles()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "Match" } };
+
+			var result = credits.GetRoles(new List<string>() { "Match" });
+
+			Assert.Single(result);
+		}
+
+		[Fact]
+		public void GetRoles_WithNoMatchingRoles_ReturnsEmpty()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "NoMatch" } };
+
+			var result = credits.GetRoles(new List<string>() { "Match" });
+
+			Assert.Empty(result);
+		}
+
+		[Fact]
+		public void GetDirectors_WithDirectors_ReturnsDirectors()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "Director" } };
+
+			var result = credits.GetDirectors();
+
+			Assert.Single(result);
+		}
+
+		[Fact]
+		public void GetDirectors_WithNoDiretors_ReturnsEmpty()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "NoMatch" } };
+
+			var result = credits.GetDirectors();
+
+			Assert.Empty(result);
+		}
+
+		[Fact]
+		public void GetWriters_WithWriter_ReturnsWriters()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "Writer" } };
+
+			var result = credits.GetWriters();
+
+			Assert.Single(result);
+		}
+
+		[Fact]
+		public void GetWriters_WithScreenplay_ReturnsWriterS()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "Screenplay" } };
+
+			var result = credits.GetWriters();
+
+			Assert.Single(result);
+		}
+
+		[Fact]
+		public void GetWriters_WithNoWriters_ReturnsEmpty()
+		{
+			var credits = new List<Credit>() { new Credit() { Role = "NoMatch" } };
+
+			var result = credits.GetWriters();
+
+			Assert.Empty(result);
+		}
+	}
+}
