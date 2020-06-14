2921bc9a71280d64849af9cde1a085d792c3fce6
Oronar <kirby91@gmail.com>
2020-06-14 16:54:17 -0400

    feat: #23 - Add GetCreators Credit extension method

--- Kinobot.Net/Extensions/CreditExtensions.cs
+++ Kinobot.Net/Extensions/CreditExtensions.cs
@@ -17,6 +17,11 @@ namespace Kinobot.Net.Extensions
 			return credits.GetRoles(new List<string>() { "Writer", "Screenplay" });
 		}
 
+		public static IEnumerable<Credit> GetCreators(this IEnumerable<Credit> credits)
+		{
+			return credits.GetRoles(new List<string>() { "Creator" });
+		}
+
 		public static IEnumerable<Credit> GetRoles(this IEnumerable<Credit> credits, IEnumerable<string> roles)
 		{
 			return credits.Where(credit => roles.Contains(credit.Role, StringComparer.InvariantCultureIgnoreCase));
