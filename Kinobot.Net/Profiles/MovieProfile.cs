4369e38a3820fc79c5b6f405ed04fcd457292506
Oronar <kirby91@gmail.com>
2020-11-22 01:13:53 -0500

    test: Simplify AutoMapper profile tests

--- Kinobot.Net/Profiles/MovieProfile.cs
+++ Kinobot.Net/Profiles/MovieProfile.cs
@@ -17,12 +17,21 @@ namespace Kinobot.Net.Profiles
 				.ForMember(dest => dest.RunTime, opt => opt.MapFrom(src => TimeSpan.FromMinutes((double)src.Runtime)))
 				.ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.VoteAverage))
 				.ForMember(dest => dest.Crew, opt => opt.MapFrom(src => src.Credits.Crew))
-				.ForMember(dest => dest.Cast, opt => opt.MapFrom(src => src.Credits.Cast));
+				.ForMember(dest => dest.Cast, opt => opt.MapFrom(src => src.Credits.Cast))
+				.ForMember(dest => dest.ImageUri, opt => opt.Ignore());
 
 			CreateMap<SearchMovie, Movie>()
 				.ForMember(dest => dest.TmdbId, opt => opt.MapFrom(src => src.Id))
 				.ForMember(dest => dest.Description, opt => opt.MapFrom(Src => Src.Overview))
-				.ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.VoteAverage));
+				.ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.VoteAverage))
+				.ForMember(dest => dest.ImdbId, opt => opt.Ignore())
+				.ForMember(dest => dest.ImageUri, opt => opt.Ignore())
+				.ForMember(dest => dest.Genres, opt => opt.Ignore())
+				.ForMember(dest => dest.RunTime, opt => opt.Ignore())
+				.ForMember(dest => dest.Budget, opt => opt.Ignore())
+				.ForMember(dest => dest.Revenue, opt => opt.Ignore())
+				.ForMember(dest => dest.Crew, opt => opt.Ignore())
+				.ForMember(dest => dest.Cast, opt => opt.Ignore());
 		}
 	}
 }
